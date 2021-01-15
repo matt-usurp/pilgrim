ad1b9ba10fe1f0a11b9bf4031496b185b54179ed
Matthew Rose <matt.usurp@gmail.com>
2021-01-15 18:14:02 +0000

    chore: eslint jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'jest',
   ],
 
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
   ],
+
+
+  env: {
+    'jest/globals': true,
+  },
 };
