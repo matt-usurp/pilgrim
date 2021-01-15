a7b947a0d4e86d2400c0eb01d1299dfefa8d8008
Matthew Rose <matt.usurp@gmail.com>
2021-01-15 21:26:06 +0000

    chore: more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,4 +18,32 @@ module.exports = {
   env: {
     'jest/globals': true,
   },
+
+  rules: {
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
+
+    'comma-spacing': 'off',
+    '@typescript-eslint/comma-spacing': ['error'],
+
+    'no-throw-literal': 'off',
+    '@typescript-eslint/no-throw-literal': ['error'],
+
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+
+    ///
+    ///
+    ///
+
+    '@typescript-eslint/prefer-nullish-coalescing': ['error'],
+    '@typescript-eslint/prefer-readonly': ['error'],
+    '@typescript-eslint/promise-function-async': ['error'],
+  },
 };
