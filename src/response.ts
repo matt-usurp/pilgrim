a55021450e1515d03c66b691ef10c47ef7256e9d
Matthew Rose <matt.usurp@gmail.com>
2021-01-29 14:00:23 +0000

    feat: switch responses to pilgrim main namespace

--- src/response.ts
+++ src/response.ts
@@ -1,16 +1,16 @@
-import { PilgrimResponse } from './application/response';
+import { Pilgrim } from './main';
 
 /**
  * Create a factory for the given response type.
  */
-export function factory<R extends PilgrimResponse.Response.Constraint>(type: R['type']): (value: R['value']) => R {
+export function factory<R extends Pilgrim.Response.Constraint>(type: R['type']): (value: R['value']) => R {
   return (value) => create(type, value);
 }
 
 /**
  * Construct a response of the given response type.
  */
-export function create<R extends PilgrimResponse.Response.Constraint>(
+export function create<R extends Pilgrim.Response.Constraint>(
   type: R['type'],
   value: R['value'],
 ): R {
@@ -20,22 +20,22 @@ export function create<R extends PilgrimResponse.Response.Constraint>(
 /**
  * Unwrap the given response and return its value.
  */
-export function unwrap<R extends PilgrimResponse.Response.Constraint>(response: R): R['value'] {
+export function unwrap<R extends Pilgrim.Response.Constraint>(response: R): R['value'] {
   return response.value;
+}
+
+/**
+ * Construct a nothing response.
+ */
+export function nothing(): Pilgrim.Response.Nothing {
+  return { type: 'nothing' } as Pilgrim.Response.Nothing;
 }
 
 /**
  * A http response.
  */
-export function http<R extends PilgrimResponse.Preset.Http>(value: R['value']): R {
+export function http<R extends Pilgrim.Response.Http>(value: R['value']): R {
   return create<R>('http', value);
-}
-
-/**
- * Construct a nothing resposne.
- */
-export function nothing(): PilgrimResponse.Preset.Nothing {
-  return { type: 'nothing' } as PilgrimResponse.Preset.Nothing;
 }
 
 /**
